7e622f345cc8bb26566055e1abfcfbe393377042
Dominibus <domenicodeluca11@gmail.com>
2026-03-12 22:31:56 +0100

    Update scrape-immobiliare.js

--- netlify/functions/scrape-immobiliare.js
+++ netlify/functions/scrape-immobiliare.js
@@ -15,9 +15,18 @@ const response = await fetch(url,{
 headers:{
 "User-Agent":
 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122 Safari/537.36",
+
 "Accept":
 "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-"Accept-Language":"it-IT,it;q=0.9,en;q=0.8"
+
+"Accept-Language":
+"it-IT,it;q=0.9,en;q=0.8",
+
+"Referer":
+"https://www.google.com/",
+
+"Cache-Control":
+"no-cache"
 }
 });
 
