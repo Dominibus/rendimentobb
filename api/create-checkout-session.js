c0abb211fc0105b7c37f82cf299c8843cb3ae846
Dominibus <domenicodeluca11@gmail.com>
2026-03-27 16:41:07 +0100

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -18,9 +18,9 @@ export default async function handler(req, res) {
 
     // 🔥 PREZZI STRIPE (USA I TUOI price_xxx)
     const prices = {
-      investor: "price_INVESTOR_ID",
-      pro: "price_PRO_ID",
-      pro_yearly: "price_YEARLY_ID"
+      investor: "prod_U8kCev2n7lTlVR",
+      pro: "prod_UAyXmZ487wFiK0",
+      pro_yearly: "prod_UAyZR28CgVdqbK"
     };
 
     const priceId = prices[plan];
