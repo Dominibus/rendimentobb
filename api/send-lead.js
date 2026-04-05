ab82e7c4bfb00fd2a369be0f92c316d208d7016a
Dominibus <domenicodeluca11@gmail.com>
2026-04-06 01:07:32 +0200

    Update send-lead.js

--- api/send-lead.js
+++ api/send-lead.js
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import admin from "firebase-admin";
 
-// ================= RESEND =================
+// ================= RESEND ================= 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 // ================= FIREBASE INIT =================
