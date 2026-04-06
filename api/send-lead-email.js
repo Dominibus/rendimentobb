0b6e7970f9426b6d8ffa4aa5b9ecfb347b895b54
Dominibus <domenicodeluca11@gmail.com>
2026-04-06 19:44:01 +0200

    Update send-lead-email.js

--- api/send-lead-email.js
+++ api/send-lead-email.js
@@ -221,7 +221,7 @@ export default async function handler(req, res) {
 
     // ================= SEND =================
     await resend.emails.send({
-      from: "RendimentoBB <noreply@rendimentobb.it>",
+      from: "RendimentoBB <analisi@rendimentobb.it>",
       to: [email],
       subject: selected.subject,
       html: selected.html
