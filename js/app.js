f5f555729bda42df7ce290b152213079ee73cfe5
Dominibus <domenicodeluca11@gmail.com>
2026-04-22 11:27:46 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3725,6 +3725,7 @@ function removeGhostOverlays(){
 document.addEventListener("rb_plan_loaded", () => {
 
   console.log("🚀 PLAN LOADED → FULL SYNC");
+  document.body.classList.remove("app-loading");
   document.body.style.visibility = "visible";
 
   const access = window.getUserAccess();
