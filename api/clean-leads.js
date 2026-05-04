7102b5e0c84132d47e2f5f6a5da06e495106c405
Dominibus <domenicodeluca11@gmail.com>
2026-05-04 15:53:48 +0200

    Create clean-leads.js

---
+++ api/clean-leads.js
@@ -0,0 +1,36 @@
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    })
+  });
+}
+
+const db = admin.firestore();
+
+export default async function handler(req, res){
+
+  try{
+
+    const snapshot = await db.collection("leads").get();
+
+    const batch = db.batch();
+
+    snapshot.docs.forEach(doc => {
+      batch.delete(doc.ref);
+    });
+
+    await batch.commit();
+
+    res.status(200).json({ success:true, deleted: snapshot.size });
+
+  }catch(e){
+    console.error(e);
+    res.status(500).json({ error:"fail" });
+  }
+
+}
