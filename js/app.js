46e7b2f5e75351a7245a2662420a2b593dc1064a
Dominibus <domenicodeluca11@gmail.com>
2026-05-29 17:59:36 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2575,17 +2575,6 @@ if(shouldSave){
           ?.value
           ?.trim();
 
-      console.log(
-  "🔥 SAVE DATA",
-  {
-    selectedCity,
-    marketCity,
-    currentCity,
-    customLocation:
-      document.getElementById("custom-location")?.value
-  }
-);
-
       saveAnalysis({
         propertyPrice: propertyPrice,
         equity,
