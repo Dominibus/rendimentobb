fcc74a47181f813f338f29a39626aacd28753179
Dominibus <domenicodeluca11@gmail.com>
2026-06-09 23:31:17 +0200

    Create property-database.js

---
+++ js/property-database.js
@@ -0,0 +1,63 @@
+window.rbPropertyDatabase = [
+
+{
+id:1,
+city:"napoli",
+title:"Monolocale Centro Storico",
+price:255000,
+sqm:44,
+roi:17,
+portal:"Immobiliare.it",
+image:"/img/napoli-1.jpg",
+url:"https://www.immobiliare.it/vendita-case/napoli/"
+},
+
+{
+id:2,
+city:"napoli",
+title:"Bilocale Chiaia",
+price:320000,
+sqm:80,
+roi:15,
+portal:"Idealista",
+image:"/img/napoli-2.jpg",
+url:"https://www.idealista.it/vendita-case/napoli/"
+},
+
+{
+id:3,
+city:"roma",
+title:"Bilocale Trastevere",
+price:380000,
+sqm:60,
+roi:14,
+portal:"Immobiliare.it",
+image:"/img/roma-1.jpg",
+url:"https://www.immobiliare.it/vendita-case/roma/"
+},
+
+{
+id:4,
+city:"milano",
+title:"Bilocale Navigli",
+price:280000,
+sqm:55,
+roi:15,
+portal:"Immobiliare.it",
+image:"/img/milano-1.jpg",
+url:"https://www.immobiliare.it/vendita-case/milano/"
+},
+
+{
+id:5,
+city:"firenze",
+title:"Bilocale Centro Storico",
+price:280000,
+sqm:50,
+roi:13,
+portal:"Immobiliare.it",
+image:"/img/firenze-1.jpg",
+url:"https://www.immobiliare.it/vendita-case/firenze/"
+}
+
+];
