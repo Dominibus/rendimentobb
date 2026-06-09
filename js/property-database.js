6778cb1b8fa890d175fd27869e071c4a80f6f728
Dominibus <domenicodeluca11@gmail.com>
2026-06-09 23:47:34 +0200

    Update property-database.js

--- js/property-database.js
+++ js/property-database.js
@@ -1,63 +1,123 @@
 window.rbPropertyDatabase = [
 
+/* ================= NAPOLI ================= */
+
 {
 id:1,
 city:"napoli",
-title:"Monolocale Centro Storico",
-price:255000,
-sqm:44,
+titleIT:"Monolocale Centro Storico",
+titleEN:"Historic Center Studio",
+price:180000,
+sqm:65,
 roi:17,
+risk:3,
 portal:"Immobiliare.it",
-image:"/img/napoli-1.jpg",
+image:"/img/cities/napoli.jpg",
 url:"https://www.immobiliare.it/vendita-case/napoli/"
 },
 
 {
 id:2,
 city:"napoli",
-title:"Bilocale Chiaia",
-price:320000,
-sqm:80,
-roi:15,
+titleIT:"Bilocale Chiaia",
+titleEN:"Chiaia Apartment",
+price:240000,
+sqm:85,
+roi:14.5,
+risk:4,
 portal:"Idealista",
-image:"/img/napoli-2.jpg",
+image:"/img/cities/napoli.jpg",
 url:"https://www.idealista.it/vendita-case/napoli/"
 },
+
+/* ================= ROMA ================= */
 
 {
 id:3,
 city:"roma",
-title:"Bilocale Trastevere",
-price:380000,
-sqm:60,
-roi:14,
+titleIT:"Bilocale Trastevere",
+titleEN:"Trastevere Apartment",
+price:290000,
+sqm:70,
+roi:13,
+risk:2,
 portal:"Immobiliare.it",
-image:"/img/roma-1.jpg",
+image:"/img/cities/roma.jpg",
 url:"https://www.immobiliare.it/vendita-case/roma/"
 },
 
 {
 id:4,
-city:"milano",
-title:"Bilocale Navigli",
-price:280000,
-sqm:55,
-roi:15,
-portal:"Immobiliare.it",
-image:"/img/milano-1.jpg",
-url:"https://www.immobiliare.it/vendita-case/milano/"
+city:"roma",
+titleIT:"Centro Storico Premium",
+titleEN:"Historic Center Premium",
+price:340000,
+sqm:95,
+roi:11.5,
+risk:2,
+portal:"Idealista",
+image:"/img/cities/roma.jpg",
+url:"https://www.idealista.it/vendita-case/roma/"
 },
+
+/* ================= FIRENZE ================= */
 
 {
 id:5,
 city:"firenze",
-title:"Bilocale Centro Storico",
-price:280000,
-sqm:50,
-roi:13,
+titleIT:"Centro Storico Firenze",
+titleEN:"Florence Historic Center",
+price:260000,
+sqm:60,
+roi:12,
+risk:2,
 portal:"Immobiliare.it",
-image:"/img/firenze-1.jpg",
+image:"/img/cities/firenze.jpg",
 url:"https://www.immobiliare.it/vendita-case/firenze/"
+},
+
+{
+id:6,
+city:"firenze",
+titleIT:"Zona Duomo",
+titleEN:"Duomo Area",
+price:320000,
+sqm:80,
+roi:11,
+risk:2,
+portal:"Idealista",
+image:"/img/cities/firenze.jpg",
+url:"https://www.idealista.it/vendita-case/firenze/"
+},
+
+/* ================= MILANO ================= */
+
+{
+id:7,
+city:"milano",
+titleIT:"Navigli Business",
+titleEN:"Navigli Business Area",
+price:350000,
+sqm:60,
+roi:10,
+risk:3,
+portal:"Immobiliare.it",
+image:"/img/cities/milano.jpg",
+url:"https://www.immobiliare.it/vendita-case/milano/"
+},
+
+{
+id:8,
+city:"milano",
+titleIT:"Porta Nuova Premium",
+titleEN:"Porta Nuova Premium",
+price:420000,
+sqm:90,
+roi:9,
+risk:3,
+portal:"Idealista",
+image:"/img/cities/milano.jpg",
+url:"https://www.idealista.it/vendita-case/milano/"
 }
 
 ];
