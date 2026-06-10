f0194e81de1bff3aaa9bdf60165e6fe48dd19645
Dominibus <domenicodeluca11@gmail.com>
2026-06-10 16:20:37 +0200

    Update property-search.js

--- api/property-search.js
+++ api/property-search.js
@@ -1,1 +1,15 @@
+export default async function handler(req, res) {
 
+  res.status(200).json({
+
+    success: true,
+
+    message: "Property Search API Ready",
+
+    city: req.query.city || null,
+
+    budget: req.query.budget || null
+
+  });
+
+}
