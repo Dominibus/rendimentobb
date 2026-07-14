3533412108b711f0ea91761438855df0795117d7
Dominibus <domenicodeluca11@gmail.com>
2026-07-14 12:19:49 +0200

    Update chatbot-loader.js

--- js/chatbot-loader.js
+++ js/chatbot-loader.js
@@ -225,6 +225,7 @@
     "/js/chatbot/core/score-engine.js",
     "/js/chatbot/document-engine.js",
     "/js/chatbot/document-reasoning-engine.js",
+    "/js/chatbot/executive-narrative-engine.js",
     "/js/chatbot/reasoning-engine.js",
     "/js/chatbot/core/response-engine.js",
     "/js/chatbot/core/chatbot-orchestrator.js",
