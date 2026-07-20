2069d442b258ea3680e2a629b53755407748ed96
Dominibus <domenicodeluca11@gmail.com>
2026-07-20 14:55:56 +0200

    Update chatbot-orchestrator.js

--- js/chatbot/core/chatbot-orchestrator.js
+++ js/chatbot/core/chatbot-orchestrator.js
@@ -853,7 +853,7 @@ const brain =
             {},
 
         documentKnowledge:
-            bestKnowledge?.item || {},
+    {},
 
         executiveContext:
             window.executiveContext || {}
