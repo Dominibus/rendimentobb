bad167ad0c45775bc362801a73e221534acd52fd
Dominibus <domenicodeluca11@gmail.com>
2026-07-21 10:56:52 +0200

    Update response-engine.js

--- js/chatbot/core/response-engine.js
+++ js/chatbot/core/response-engine.js
@@ -69,7 +69,7 @@ console.log(
         topic,
         isFollowUp,
         isShortQuestion,
-        hasAnalysis,
+        conversationHasAnalysis,
         contextConfidence,
         originalMessage
     }
