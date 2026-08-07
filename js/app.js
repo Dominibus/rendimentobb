863bcbaa70d2d8e407ed36aa21d8c3543db44843
Dominibus <domenicodeluca11@gmail.com>
2026-08-07 15:42:57 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3655,8 +3655,7 @@ window.__LAST_CALCULATION__ = 0;
 
 window.calculate = async function(mode = false){
 
-  console.trace("🔥 CALCULATE SOURCE", mode);
-
+ 
   const isUIRefresh =
   mode === "ui_refresh";
 
