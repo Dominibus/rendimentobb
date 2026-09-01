4e4a2eda3097fe774d6d2157cc6f8f1a6f3e1c44
Dominibus <domenicodeluca11@gmail.com>
2026-09-01 11:58:43 +0200

    Update send-lead-email.js

--- api/send-lead-email.js
+++ api/send-lead-email.js
@@ -10,8 +10,6 @@ export default async function handler(req, res){
 
   try{
 
-    console.log("⚠️ send-lead-email chiamato → redirect a send-lead");
-
     const response = await fetch(
       `${process.env.BASE_URL || "https://rendimentobb.it"}/api/send-lead`,
       {
