5a043f91b355f04f86778ee15e3a155f44115b20
Dominibus <domenicodeluca11@gmail.com>
2026-09-08 16:43:17 +0200

    Delete api/clean-leads.js

--- api/clean-leads.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Cache-Control", "no-store");
-
-  return res.status(410).json({
-    success: false,
-    error: "endpoint_disabled"
-  });
-}
